8885ef369c186a9cb3dc944f04c1e482b1352cb3
Tony Yang <tianyuyang658@yahoo.com>
2020-04-19 14:23:13 -0700

    Start adding layout to the page. Store user connection so only a single user can draw. Normalize the drawing so different device size can see everything

--- index.js
+++ index.js
@@ -17,7 +17,33 @@ app.get('/', (req, res) => {
 
 var lines = [];
 var messages = [];
+// users store the user id and whether the user is drawing or not
+// true == drawing, false == not drawing
+var users = {};
+// record joint order to decide who will draw next
+// only the server needs the jointOrder to decide who's drawing
+var jointOrder = []
+
+function registerUser(socket) {
+  socket.on('new_user', (u) => {
+    console.log('receive new user =', u);
+    console.log('users = ', users);
+    console.log('users.length =', users.length);
+    if (Object.keys(users).length == 0) {
+      users[u] = true;
+    } else {
+      users[u] = false;
+    }
+    jointOrder.push(u);
+    console.log('users =', users);
+    console.log('join order =', jointOrder);
+    io.emit('new_user', users);
+  });
+}
+
 io.on('connection', (socket) => {
+  registerUser(socket);
+
   for (let l of lines) {
     socket.emit('draw_line', {line: l});
   }
--- public/draw.js
+++ public/draw.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   var socket = io();
 
+  // Register the current user first
+  var username = Math.random().toString(36).slice(2);
+  console.log('username =', username)
+  socket.emit('new_user', username);
+
   var mouse = {
     click: false,
     move: false,
@@ -10,30 +15,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var canvas = document.getElementById('board');
   var context = canvas.getContext('2d');
-  var width = 500;
-  var height = 500;
+  var height = 400;
 
-  canvas.width = width;
-  canvas.hegiht = height;
+  var canvasDiv = document.getElementById('canvas')
+  canvas.width = canvasDiv.clientWidth;
+  canvas.hegiht = canvasDiv.clientHeight;
 
   canvas.onmousedown = function(e) {mouse.click = true;}
   canvas.onmouseup = function(e) {mouse.click = false;}
+  // Divide the canvas size to normalize the drawing to unit length.
   canvas.onmousemove = function(e) {
-    mouse.pos.x = e.offsetX;
-    mouse.pos.y = e.offsetY;
+    mouse.pos.x = e.offsetX / canvas.width;
+    mouse.pos.y = e.offsetY / canvas.height;
     mouse.move = true;
   };
+
+  var allUsers = [];
 
   socket.on('draw_line', (d) => {
     var line = d.line;
     context.beginPath();
-    context.moveTo(line[0].x, line[0].y);
-    context.lineTo(line[1].x, line[1].y);
+    // Multiply the canvas size to restore from unit length to device canvas
+    // size in proportion.
+    context.moveTo(line[0].x * canvas.width, line[0].y * canvas.height);
+    context.lineTo(line[1].x * canvas.width, line[1].y * canvas.height);
     context.stroke();
   });
 
+  socket.on('new_user', (u) => {
+    allUsers = u;
+  })
+
+  // Only allow one user to draw at a time. Everyone else are guessing.
   function mainLoop() {
-    if (mouse.click && mouse.move && mouse.pos_prev) {
+    if (allUsers[username] && mouse.click && mouse.move && mouse.pos_prev) {
       socket.emit('draw_line', {line: [mouse.pos, mouse.pos_prev]});
       mouse.move = false;
     }
